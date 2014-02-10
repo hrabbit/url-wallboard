e20e20bcf2a268e4766cc8cf05cf848bae23255c
Scott Horsley <scott@horsley.id.au>
2014-02-10 18:26:37 +1100

    Updated system to remove widget titles

--- src/Hpbx/Helper.php
+++ src/Hpbx/Helper.php
@@ -17,8 +17,8 @@ class Helper
 
 	public function init_database()
 	{
-		\QB::query('CREATE TABLE IF NOT EXISTS users( id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR(30) UNIQUE, role VARCHAR(30), password VARCHAR(255))');
-		\QB::query('INSERT OR IGNORE INTO users (user,role,password) VALUES ("admin", "ROLE_ADMIN", "5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg==")');
+		\QB::query('CREATE TABLE IF NOT EXISTS users( id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR(30) UNIQUE, password VARCHAR(255))');
+		\QB::query('INSERT OR IGNORE INTO users (user,role,password) VALUES ("admin", 5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg==")');
 		\QB::query('CREATE TABLE IF NOT EXISTS options( id INTEGER PRIMARY KEY AUTOINCREMENT, key VARCHAR(30) UNIQUE, value VARCHAR(255), required BOLEAN default 0)');
 		\QB::query('INSERT OR IGNORE INTO options (key,value,required) VALUES ("title", "My wallboard", 1)');
 		\QB::query('INSERT OR IGNORE INTO options (key,value,required) VALUES ("widgets_per_row", "3", 1)');
@@ -29,7 +29,7 @@ class Helper
 		\QB::query('INSERT OR IGNORE INTO options (key,value,required) VALUES ("widget_forground_color", "#FFFFFF", 1)');
 		\QB::query('INSERT OR IGNORE INTO options (key,value,required) VALUES ("threshold_wait_time", "60", 1)');
 		\QB::query('INSERT OR IGNORE INTO options (key,value,required) VALUES ("threshold_calls_waiting", "3", 1)');
-		\QB::query('CREATE TABLE IF NOT EXISTS widgets( id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(30), url VARCHAR(255) UNIQUE)');
+		\QB::query('CREATE TABLE IF NOT EXISTS widgets( id INTEGER PRIMARY KEY AUTOINCREMENT, url VARCHAR(255) UNIQUE)');
 		\QB::query('CREATE TABLE IF NOT EXISTS pages( id INTEGER PRIMARY KEY AUTOINCREMENT, url VARCHAR(255) UNIQUE)');
 	}
 
